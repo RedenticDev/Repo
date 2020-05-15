f25d35ddcb06c12842d200f429cf3e29dc44e2d9
RedenticDev <redenticdev@gmail.com>
2020-05-15 23:10:34 +0200

    fixes

--- depictions/js/setDepiction.js
+++ depictions/js/setDepiction.js
@@ -1,8 +1,7 @@
-
-$(function(){
+$(function() {
     var bundle = getQueryVariable('p');
 
-    if(bundle != undefined){
+    if (bundle != undefined){
         //Now fetch the appropriate file from this query string
     }
 
@@ -10,7 +9,7 @@ $(function(){
     console.log(getQueryVariable('p'));
     console.log("Fetching XML");
     var getUrl = window.location;
-    var baseUrl = getUrl .protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
+    var baseUrl = getUrl.protocol + "//" + getUrl.host + "/" + getUrl.pathname.split('/')[1];
     console.log(baseUrl + "/"+ bundle + "/info.xml");
 
     $.ajax({
@@ -21,40 +20,35 @@ $(function(){
             console.log("Beginning XML Parsing");
 
             // Parse the xml file and get data
-            $(xml).find('packageInfo').each(function(){
-                  // document.getElementById("packageTitle").innerHTML = $(this).find("name").text();
-                   //document.getElementById("bundleId").innerHTML = $(this).find("bundleId").text();
-                   //document.getElementById("version").innerHTML = $(this).find("version").text();
-                   document.getElementById("miniOS").innerHTML = $(this).find("miniOS").text();
-                   document.getElementById("maxiOS").innerHTML = $(this).find("maxiOS").text();
+            $(xml).find('packageInfo').each(function() {
+                // document.getElementById("packageTitle").innerHTML = $(this).find("name").text();
+                // document.getElementById("bundleId").innerHTML = $(this).find("bundleId").text();
+                // document.getElementById("version").innerHTML = $(this).find("version").text();
+                document.getElementById("miniOS").innerHTML = $(this).find("miniOS").text();
+                document.getElementById("maxiOS").innerHTML = $(this).find("maxiOS").text();
 
-                   $(xml).find('description').each(function(){
-                       $("#description" ).append('<li>' +$(this).text()+ '</li>');
-                   });
+                $(xml).find('description').each(function() {
+                    $("#description").append('<li>' + $(this).text() + '</li>');
+                });
 
-                   $(xml).find('dependency').each(function(){
-                       $("#dependencies" ).append('<li>' +$(this).text()+ '</li>');
-                   });
+                $(xml).find('dependency').each(function() {
+                    $("#dependencies").append('<li>' + $(this).text() + '</li>');
+                });
 
-                   $(xml).find('linkName').each(function(){
-                       $("#dependencies" ).append('<li>' +$(this).text()+ '</li>');
-                   });
+                $(xml).find('linkName').each(function() {
+                    $("#dependencies").append('<li>' + $(this).text() + '</li>');
+                });
 
-                   $(xml).find('change').each(function(){
-                       $("#changeLog" ).append('<li>' + '<h1>' + $(this).find("changeVersion").text() + '</h1>');
-                       $(this).find('changeDescription').each(function(){
-                            $("#changeLog" ).append('<h2>' + $(this).text()+ '<h2>');
-                       });
-                       $("#changeLog" ).append('<li>');
-                   });
-
-
+                $(xml).find('change').each(function() {
+                    $("#changeLog").append('<li><h1>' + $(this).find("changeVersion").text() + '</h1>');
+                    $(this).find('changeDescription').each(function() {
+                        $("#changeLog").append('<h2>' + $(this).text() + '<h2>');
+                    });
+                    $("#changeLog").append('<li>');
+                });
             });
-
         }
     });
-
-
 });
 
 
