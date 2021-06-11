d9bef6b3c8a09854e2c3764d9ebf3321bd609c06
RedenticDev <redenticdev@gmail.com>
2021-06-11 13:34:42 +0200

    Compliance improvements, bug fixes

--- scrollToTop.js
+++ scrollToTop.js
@@ -5,18 +5,22 @@
  window.addEventListener("load", function () {
 	var btn = document.getElementsByClassName("top")[0];
 	if (btn) {
-		btn.addEventListener("click", function(e) {
+		// Click to scroll to top
+		btn.addEventListener("click", function (e) {
 			e.preventDefault();
 			scroll({ top: 0, left: 0, behavior: 'smooth' });
 		});
-		if (window.scrollY >= 100 && window.getComputedStyle(btn, null).getPropertyValue('right').replace(/px$/, '') <= 0) {
+		// Triggers button if refresh occurs while not being at the top of the page
+		if (window.scrollY >= 10 && window.getComputedStyle(btn, null).getPropertyValue('right').replace(/px$/, '') <= 0) {
 			btn.style.right = "30px";
 			btn.style.transform = "none";
 		}
-		window.addEventListener("scroll", function() {
+		// Scroll dynamic triggering
+		window.addEventListener("scroll", function () {
+			if (window.scrollY < 0) return;
 			var btnRight = window.getComputedStyle(btn, null)
-				.getPropertyValue('right')
-				.replace(/px$/, '')
+			.getPropertyValue('right')
+			.replace(/px$/, '');
 			if (window.scrollY >= 10) {
 				if (btnRight <= 0) {
 					btn.style.right = "30px";
