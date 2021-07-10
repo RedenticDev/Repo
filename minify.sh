9978820f81d541a5185ec5396fa562eb51b17bd1
RedenticDev <redenticdev@gmail.com>
2021-07-10 20:40:06 +0200

    [Website/Repo] Fix mangling too hard

--- minify.sh
+++ minify.sh
@@ -1,5 +1,5 @@
 # Minify JavaScript files
-find . -name "*.js" ! -name "*.min.*" -exec sh -c 'uglifyjs -c -m --mangle-props -o "${0%.js}.min.js" "$0"' {} \;
+find . -name "*.js" ! -name "*.min.*" -exec sh -c 'uglifyjs -c -m -o "${0%.js}.min.js" "$0"' {} \;
 
 # Minify CSS files
 find . -name "*.css" ! -name "*.min.*" -exec sh -c 'uglifycss --output "${0%.css}.min.css" "$0"' {} \;
