6076640cd6365da889dff35c48514971e4db3b08
RedenticDev <redenticdev@gmail.com>
2021-08-09 18:43:42 +0200

    [META] Add public key, remove script and a release

--- minify.sh
+++
@@ -1,5 +0,0 @@
-# Minify JavaScript files
-find . -name "*.js" ! -name "*.min.*" -exec sh -c 'uglifyjs -c -m -o "${0%.js}.min.js" "$0"' {} \;
-
-# Minify CSS files
-find . -name "*.css" ! -name "*.min.*" -exec sh -c 'uglifycss --output "${0%.css}.min.css" "$0"' {} \;
