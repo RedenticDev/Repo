f65d10836731c8c057542f5065aede0066bd1424
RedenticDev <redenticdev@gmail.com>
2021-12-11 18:39:31 +0100

    [Website] Devices improvements - Remove pointless devices - Add caption to table - Add `<th>` tag for accessibility - Slightly adjust collapsible button' height

--- js/script.js
+++ js/script.js
@@ -106,9 +106,7 @@ document.addEventListener("readystatechange", () => {
         ["checkra1n", "iPhone 6s", "iOS 13.7", "128 GB", "Space Gray"],
         ["checkra1n", "iPhone 6", "iOS 12.5.5", "64 GB", "Gold"],
         ["checkra1n", "iPad 6", "iPadOS 14.8", "32 GB", "Gold"],
-        ["MacBook Pro 2021", "macOS 12.0.1 Monterey", "M1 Pro 10C/16C", "16 GB / 1 TB", "Space Gray"],
-        ["Apple Watch Series 5", "watchOS 8.1", "Space Gray"],
-        ["AirPods Pro", "Wireless Case"]
+        ["MacBook Pro 2021", "macOS 12.0.1 Monterey", "M1 Pro 10C/16C", "16 GB / 1 TB", "Space Gray"]
     ];
 
     function wordToImage(word) {
